35a1a80e8a6dcb0a347b1a8c2948442d42a75866
Janardhana <janachet@amazon.com>
2022-11-04 11:10:14 +0530

    POrt changes

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({extended:true}));
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, ()=>{
-    console.log("App listening on port 4000")
+    console.log("App listening on port", PORT)
 })
 
 var indexRouter = require('./routes/index');
