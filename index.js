b74dd82a7150d9d431958f8eb79d93599cb07078
Janardhana <janachet@amazon.com>
2022-11-04 10:53:51 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const ejs = require('ejs')
+
+const app = express();
+
+app.set('view engine','ejs');
+
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()=>{
+    console.log("App listening on port 4000")
+})
+
+var indexRouter = require('./routes/index');
+
+app.use('/', indexRouter);
